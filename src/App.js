706712404c62b4ff40d55db2390283d08de04bba
Matt Priston <mpristoncarruthersfi@ucsb.edu>
2023-01-18 12:56:54 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './main.css';
+
+const App = () => {
+    return(
+        <>
+        <h1>Matt Priston - Hello World Webapp</h1>
+        <body>This is the Hello world page that needed to be created for the Lab1 of CS148 </body>
+        <h2>A brief description about what our prject is going to be:</h2>
+        <body>In our project we will create a 4-year scheduler planner that students can use to plan all their future classes as well as being able to see which classes they still need to copmpolete in order to graduate</body>
+        </>
+    );
 }
 
 export default App;
